982a24dfcba291ba97f29e87354a1064449af8b0
gilberto <gilberto_aquarius@hotmail.com>
2015-05-15 18:37:25 -0500

    mvc de administrador full y primeros mvc de administracion usuarios

--- app/controllers/administrador.js
+++ app/controllers/administrador.js
@@ -1,10 +1,23 @@
+var args = arguments[0] || {};
+
 $.btnusuarios.addEventListener('click',function(){
-	// Alloy.createController('usuarios');
-	alert("Vista usuarios");
+	Alloy.createController('usuarios');
+	// alert("Vista usuarios");
 });
 $.btnarticulos.addEventListener('click',function(){
 	// Alloy.createController('usuarios');
 	alert("Vista articulos");
 });
 
+function volver(){
+	$.winadmin.close();
+};
+
+(function(){
+	console.log(JSON.stringify(args));
+	// console.log("----------->>>"+args.nombre);
+	$.nombre.setText(args.nombre+" "+args.ap_paterno);
+	$.cargo.setText(args.cargo);
+	$.empleado.setText(args.num_empleado);
+})();
 $.winadmin.open();
