7f9f945b4df051400782ae8235cefd1d95e82c87
roco93 <respinoza140@gmail.com>
2015-05-15 16:45:23 -0500

    mvc adminitrador agregado

---
+++ app/controllers/administrador.js
@@ -0,0 +1,10 @@
+// $.btnusuarios.addEventListener('click',function(){
+	// // Alloy.createController('usuarios');
+	// alert("Vista usuarios");
+// });
+// $.btnarticulos.addEventListener('click',function(){
+	// // Alloy.createController('usuarios');
+	// alert("Vista articulos");
+// });
+
+$.winadmin.open();
